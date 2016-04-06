2339d45641705ad7f862f591a70ca5cd4187f75e
talpasco <talpasco@gmail.com>
2016-04-06 13:46:36 +0300

    MySQL configuration
    1) Modifyed the datasources so 'db' is MySQL
    2) Added loopback-connector-mysql to 'package.json'
    3) Added 'autoupdate.js' to the boot sequence so MySQL server will be
    updated.
    4) Had to hide\comment the code inside "boot\sample-models.js" in order
    to update MySQL.
    5) run node .
    6) it will fail at the first time... so run it again.
    7) unComment the code inside "boot\sample-models.js"
    8) now you can run 'node .' without missing anything, but it would fail
    at the creation of users relations:
    Running this example on memory DB, it runs perfectly.
    But, using MySQL as data-source, produce the following error:
    
    C:\loopback-example-access-control\node_modules\mysql\lib\protocol\Parser.js:77
    throw err; // Rethrow non-MySQL errors
    ^
    ValidationError: The user instance is not valid. Details: email is
    invalid (value: "john@doe.com"); username is invalid (value:
    "John").,ValidationError: The user instance is not valid. Details: email
    is invalid (value: "jane@doe.com"); username is invalid (value:
    "Jane").,ValidationError: The user instance is not valid. Details: email
    is invalid (value: "bob@projects.com"); username is invalid (value:
    "Bob").
    
    Many thanks for all of you who are helping with this awesome project,
    Tal

---
+++ server/boot/autoupdate.js
@@ -0,0 +1,36 @@
+﻿var path = require('path');
+var app = require(path.resolve(__dirname, '../server'));
+module.exports = function (app) {
+    var models = require(path.resolve(__dirname, '../model-config.json'));
+    var datasources = require(path.resolve(__dirname, '../datasources.json'));
+    
+    function autoUpdateAll() {
+        Object.keys(models).forEach(function (key) {
+            if (typeof models[key].dataSource != 'undefined') {
+                if (typeof datasources[models[key].dataSource] != 'undefined') {
+                    app.dataSources[models[key].dataSource].autoupdate(key, function (err) {
+                        if (err) throw err;
+                        console.log('Model ' + key + ' updated');
+                    });
+                }
+            }
+        });
+    }
+    
+    function autoMigrateAll() {
+        Object.keys(models).forEach(function (key) {
+            if (typeof models[key].dataSource != 'undefined') {
+                if (typeof datasources[models[key].dataSource] != 'undefined') {
+                    app.dataSources[models[key].dataSource].automigrate(key, function (err) {
+                        if (err) throw err;
+                        console.log('Model ' + key + ' migrated');
+                    });
+                }
+            }
+        });
+    }
+    //TODO: change to autoUpdateAll when ready for CI deployment to production
+    //autoMigrateAll();
+    autoUpdateAll();
+
+};
